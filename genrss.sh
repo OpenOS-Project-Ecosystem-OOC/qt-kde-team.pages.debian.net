4c08eea501649a59729fe1ebe27c3bf864fd0de0
Eshat Cakar <info@eshat.de>
2011-06-06 11:15:01 +0200

    Make RSS work right and valid XML. Use more actual logo on rss and fix tabs

--- genrss.sh
+++ genrss.sh
@@ -2,34 +2,29 @@
 
 FILE=pages/index
 
-NEWS_START=`grep -n "<h2>Latest" ${FILE}  | sed 's/:.*//'`
+NEWS_START=`grep -n "RSS_START" ${FILE}  | sed 's/:.*//'`
 
 let "NEWS_START += 1"
 
-NEWS_END=`tail -n +${NEWS_START} ${FILE} | grep -n "<h2>" | sed 's/:.*//'`
+NEWS_END=`tail -n +${NEWS_START} ${FILE} | grep -n "RSS_END" | sed 's/:.*//'`
 
-if [ -z "${NEWS_END}" ]
-then
-	NEWS_END=`tail -n +${NEWS_START} ${FILE} | wc --lines`
-else
-	let "NEWS_END -= 1"
-fi
+let "NEWS_END -= 1"
 
 cat <<__EOHEAD__ 
 <?xml version="1.0" encoding="iso-8859-1"?>
 <rss version="2.0">
-        <channel>
-                <title>Debian KDE maintainers</title>
-                <description>Debian KDE news feed</description>
-                <link>http://pkg-kde.alioth.debian.org/</link>
-                <lastBuildDate>`date -R`</lastBuildDate>
-                <generator>bash hacking</generator>
-                <image>
-                        <url>http://pkg-kde.alioth.debian.org/images/KDEbian2-60x60.png</url>
-                        <title>Debian KDE maintainers</title>
-                        <link>http://pkg-kde.alioth.debian.org/</link>
-                        <description>Debian KDE news feed</description>
-                </image>
+<channel>
+<title>Debian KDE maintainers</title>
+<description>Debian KDE news feed</description>
+<link>http://pkg-kde.alioth.debian.org/</link>
+<lastBuildDate>`date -R`</lastBuildDate>
+<generator>bash hacking</generator>
+<image>
+  <url>http://pkg-kde.alioth.debian.org/images/kdedebianlogo2_64.png</url>
+  <title>Debian KDE maintainers</title>
+  <link>http://pkg-kde.alioth.debian.org/</link>
+  <description>Debian KDE news feed</description>
+</image>
 __EOHEAD__
 
 tail -n +${NEWS_START} ${FILE} | head -n +${NEWS_END} | sed  's/<a[^>]*>//g' | sed 's/<\/a[^>]*>//g' | sed 's,<p>,<item>,g;s,</p>,</description></item>,g;s,<strong>,<title>,g;s,</strong>,</title>,g;s,<br />,<link>http://pkg-kde.alioth.debian.org/</link><description>,g'
